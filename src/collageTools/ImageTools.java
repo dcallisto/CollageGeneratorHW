718e5fe0ad78806ed6b21391f60f33c3ff526266
Noah Mitsuhashi <nmitsuha@usc.edu>
2018-02-25 18:09:43 -0800

    Update ImageTools.java
    Fixed formatting

--- src/collageTools/ImageTools.java
+++ src/collageTools/ImageTools.java
@@ -72,14 +72,14 @@ public class ImageTools {
 	
 	public Collection<BufferedImage> addBorder(Collection<BufferedImage> collection){
 		JPanel gui = new JPanel(new BorderLayout());
-        // to contrast the 'picture frame' border created below
-        gui.setBorder(new LineBorder(Color.WHITE, 8));
-        for(BufferedImage bi: collection) {
-	        JLabel l = new JLabel(new ImageIcon(bi));
-	        Border b = new LineBorder(Color.WHITE, 8);
-	        l.setBorder(b);
-	        gui.add(l);
-        }
+        	// to contrast the 'picture frame' border created below
+        	gui.setBorder(new LineBorder(Color.WHITE, 8));
+        	for(BufferedImage bi: collection) {
+	        	JLabel l = new JLabel(new ImageIcon(bi));
+	        	Border b = new LineBorder(Color.WHITE, 8);
+	        	l.setBorder(b);
+	        	gui.add(l);
+        	}
 		return collection;
 	}
 	
